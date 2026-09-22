ae9ba187f3ed3cefd250a9fe173e5f3ee0de6c81
JengunSeo <tjwjddns1302@ajou.ac.kr>
2026-09-23 01:12:41 +0900

    feat: 추천 모달 식재료 선택 기능 구현

---
+++ src/features/ingredient/model/categoryMap.js
@@ -0,0 +1,8 @@
+export const INGREDIENT_CATEGORY = {
+  1: { name: '채소', icon: '🥬' },
+  2: { name: '과일', icon: '🍎' },
+  3: { name: '육류', icon: '🥩' },
+  4: { name: '수산물', icon: '🐟' },
+  5: { name: '유제품', icon: '🥛' },
+  6: { name: '가공식품', icon: '🥫' },
+}

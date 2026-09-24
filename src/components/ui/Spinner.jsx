d2147100a9e846681a64d6988d6d7135aed55323
JengunSeo <tjwjddns1302@ajou.ac.kr>
2026-09-24 22:36:05 +0900

    feat: spinner 공통 컴포넌트화

---
+++ src/components/ui/Spinner.jsx
@@ -0,0 +1,18 @@
+function Spinner() {
+  return (
+    <span
+      className="
+      block 
+        h-9 
+        w-9 
+        animate-spin 
+        rounded-full 
+        border-4 
+        border-green-100 
+        border-t-green-700"
+      aria-hidden="true"
+    />
+  )
+}
+
+export default Spinner
